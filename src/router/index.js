fd1d3c93ef0f88fab29ca7cf4651867b2a55bedf
heyushuang <heyushuang@danale.com>
2020-09-04 16:16:25 +0800

    feat:日期组件;

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import List from '@/views/list'
+import Date from '@/views/date'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'List',
       component: List
+    },
+    {
+      path: '/date',
+      name: 'Date',
+      component: Date
     }
   ]
 })
---
+++ src/views/date.vue
@@ -0,0 +1,31 @@
+<template>
+    <date-picker :startTime="startTime" :endTime="endTime" :dateValue="dateValue" @getTimeData="getTimeData"></date-picker>
+</template>
+
+<script>
+export default {
+  created() {
+    this.endTime = new Date().getTime();
+    this.startTime = this.endTime - 3600 * 1000 * 24 * 7;
+  },
+  mounted() {},
+  data() {
+    return {
+      dateValue: [],
+      startTime: 0,
+      endTime: 0
+    };
+  },
+  methods: {
+    getTimeData(startTime, endTime) {
+      this.startTime = startTime;
+      this.endTime = endTime;
+      console.log(startTime);
+      console.log(endTime);
+    }
+  }
+};
+</script>
+
+<style>
+</style>
